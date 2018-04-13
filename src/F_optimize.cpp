eb23273daeefe4cdcfa691b6eb5a3766cc78f731
serfg3232 <sergei.pozdnyakov@gmail.com>
2018-04-13 23:51:39 +0300

    F_optimize

---
+++ src/F_optimize.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <eigen3/Eigen/Dense>
+
+Eigen::MatrixXd OptimizeByF(Eigen::MatrixXd Y, Eigen::MatrixXd X, double lambda) {
+    Eigen::BDCSVD<Eigen::MatrixXd> svd(Eigen::Transpose<Eigen::MatrixXd>(X),
+                                       Eigen::ComputeThinU | Eigen::ComputeFullV);
+    Eigen::MatrixXd U = svd.matrixU();
+    Eigen::MatrixXd V = svd.matrixV();
+    Eigen::VectorXd singularValues = svd.singularValues();
+    Eigen::VectorXd properValues(singularValues);
+
+    for (int i = 0; i < properValues.rows(); ++i) {
+        double now = singularValues(i);
+        properValues(i) = now / (now * now + lambda);
+    }
+
+
+    Eigen::DiagonalMatrix<double, Eigen::Dynamic> middle_d(properValues);
+    Eigen::MatrixXd middle(middle_d);
+
+
+    Eigen::MatrixXd proceededMatrix = V * middle * Eigen::Transpose<Eigen::MatrixXd>(U);
+
+    Eigen::MatrixXd result = proceededMatrix * Eigen::Transpose<Eigen::MatrixXd>(Y);
+    return  Eigen::Transpose<Eigen::MatrixXd>(result);
+
+}
+
